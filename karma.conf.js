5205f921f8269e2eb2e86923c66598eaf87324bb
Joris P <71002181+Miisterone@users.noreply.github.com>
2024-04-30 10:23:43 +0200

    Add karma

---
+++ karma.conf.js
@@ -0,0 +1,15 @@
+module.exports = function(config) {
+  config.set({
+      reporters: ['progress', 'coverage'],
+    preprocessors: {
+    '**/*.ts': ['coverage']
+  },
+  coverageReporter: {
+    dir: require('path').join(__dirname, './coverage'),
+      reporters: [
+      { type: 'html', subdir: 'report-html' },
+      { type: 'lcovonly', subdir: '.', file: 'lcov.info' }
+    ]
+  },
+});
+};

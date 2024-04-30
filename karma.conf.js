9fdbcfa1ba98057077aa7c511368f3fc03a2b24d
Joris P <71002181+Miisterone@users.noreply.github.com>
2024-04-30 11:03:11 +0200

    Full coverage

--- karma.conf.js
+++ karma.conf.js
@@ -1,22 +1,68 @@
 module.exports = function(config) {
   config.set({
-      reporters: ['progress', 'coverage'],
+    // Base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+    // Frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+
+    // List of files / patterns to load in the browser
+    files: [
+      { pattern: './src/test.ts', watched: false },
+    ],
+
+    // Preprocessors to use
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-    '**/*.ts': ['coverage']
-  },
-  coverageReporter: {
-    dir: require('path').join(__dirname, './coverage'),
-      reporters: [
-      { type: 'html', subdir: 'report-html' },
-      { type: 'lcovonly', subdir: '.', file: 'lcov.info' }
-    ]
-  },
+      './src/**/*.ts': ['coverage']
+    },
+
+    // Test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress', 'coverage'],
+
+    // Web server port
+    port: 9876,
+
+    // Enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+    // Level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+    // Enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
+    // Start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['ChromeHeadless'],
+
+    // Custom launcher for CI environments
     customLaunchers: {
       ChromeHeadlessCI: {
         base: 'ChromeHeadless',
         flags: ['--no-sandbox', '--disable-gpu']
       }
     },
-});
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: true,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity,
+
+    // Coverage reporter configuration
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage'),
+      reporters: [
+        { type: 'html', subdir: 'report-html' },
+        { type: 'lcovonly', subdir: '.', file: 'lcov.info' }
+      ]
+    }
+  });
 };

123fd4c9e181c492b60de7a4fcc1ce21116e2a87
Joris P <71002181+Miisterone@users.noreply.github.com>
2024-04-30 11:53:00 +0200

    Test

--- karma.conf.js
+++ karma.conf.js
@@ -1,15 +1,69 @@
 module.exports = function (config) {
   config.set({
-    reporters: ['progress', 'coverage'],
+    // Base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+    // Frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+
+    // List of files / patterns to load in the browser
+    files: [
+      // Patterns to load files
+    ],
+
+    // List of files / patterns to exclude
+    exclude: [
+    ],
+
+    // Preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
       'src/app/**/*.ts': ['coverage']
     },
+
+    // Test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress', 'coverage'],
+
+    // Coverage reporter configuration
     coverageReporter: {
       dir: require('path').join(__dirname, './coverage'),
       reporters: [
-        {type: 'html', subdir: 'report-html'},
-        {type: 'lcovonly', subdir: '.', file: 'lcov.info'}
+        { type: 'html', subdir: 'report-html' },
+        { type: 'lcovonly', subdir: '.', file: 'lcov.info' }
       ]
     },
+
+    // Web server port
+    port: 9876,
+
+    // Enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+    // Level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+    // Enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
+    // Start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['ChromeHeadless'],
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: true,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity,
+
+    // Configure the Angular CLI builder
+    angularCli: {
+      environment: 'dev'
+    }
   });
 };
